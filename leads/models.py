a3a5dfb2bf14d42fd86298b3a93ee0230ecde621
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-02-20 12:55:51 +0600

    Update models.py

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -8,5 +11,4 @@ class Lead(models.Model):
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
 class Agent(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
